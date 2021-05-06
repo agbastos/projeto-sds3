1b6367cd949060c8b86dfb21f7814f088c64eb60
André Gustavo Bastos <agbastos@hotmail.com>
2021-05-05 21:49:11 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = (): JSX.Element => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/agbastos" target="_blank" rel="noreferrer">André Gustavo Bastos</a></p>
+    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+  </div>
+</footer>
+    );
+}
+
+export default Footer;
