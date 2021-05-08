271924a26876df68bf5586d80a5f6e4d49b5c4f0
André Gustavo Bastos <agbastos@hotmail.com>
2021-05-07 21:24:21 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -14,14 +14,14 @@ const DonutChart = () => {
   const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
 
   useEffect(() => {
-    axios.get(`${BASE_URL} + "/sales/amount-by-seller"`)
-    .then(response => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+      .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
 
-      setChartData({ labels: myLabels, series: mySeries});
-      console.log(response.data)
+        setChartData({ labels: myLabels, series: mySeries});
+
     });
   }, []);
 
